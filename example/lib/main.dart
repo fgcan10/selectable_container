d303af924589318c9f67bad5b6704850b2cfebd4
Fernando <fgcan10@gmail.com>
2019-11-10 11:27:46 +0000

    Selectable container with opacitiy animation

---
+++ example/lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:selectable_container/selectable_container.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Selectable Container Example',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var textStyles = Theme.of(context).textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Selectable Container Example'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SelectableContainer(
+            child: buildTextContentOfContainer(
+                'Selectable Container', 'Default theme colors', textStyles),
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          SelectableContainer(
+            selectedBorderColor: Colors.teal.shade700,
+            selectedBackgroundColor: Colors.grey.shade100,
+            unselectedBorderColor: Colors.teal.shade600,
+            unselectedBackgroundColor: Colors.grey.shade200,
+            iconAlignment: Alignment.topLeft,
+            icon: Icons.thumb_up,
+            iconSize: 24,
+            unselectedOpacity: 0.5,
+            child: buildTextContentOfContainer('Custom color',
+                'Icon, size, position en opacitiy changed', textStyles),
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          SelectableContainer(
+            iconSize: 20,
+            icon: Icons.star,
+            iconColor: Colors.yellowAccent,
+            unselectedOpacity: 0.3,
+            opacityAnimationDuration: 300,
+            child: buildDemoContent3(),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+
+  Padding buildTextContentOfContainer(
+      String title, String subtitle, TextTheme textStyles) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            title,
+            style: textStyles.display1,
+          ),
+          Text(
+            subtitle,
+            style: textStyles.body1,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildDemoContent3() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('images/flutter.png'),
+                radius: 25.0,
+              ),
+              SizedBox(
+                width: 8.0,
+              ),
+              Text(
+                'Develop with Flutter',
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20.0,
+                    color: Colors.blue.shade700),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/selectable_container.dart
+++ lib/selectable_container.dart
@@ -1,7 +1,124 @@
 library selectable_container;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+/// Box that can be tapped.
+/// When selected a check Icon appears
+class SelectableContainer extends StatefulWidget {
+  Color selectedBackgroundColor;
+  Color unselectedBackgroundColor;
+  Color selectedBorderColor;
+  Color unselectedBorderColor;
+  Color iconColor;
+  int iconSize;
+  Widget child;
+  int borderSize;
+  Function onPressed;
+  double unselectedOpacity;
+  int opacityAnimationDuration;
+  IconData icon;
+  Alignment iconAlignment;
+
+  @override
+  _SelectableContainerState createState() => _SelectableContainerState();
+
+  SelectableContainer(
+      {this.unselectedBackgroundColor,
+      this.selectedBackgroundColor,
+      this.selectedBorderColor,
+      this.unselectedBorderColor,
+      @required this.onPressed,
+      this.iconSize = 16,
+      this.iconColor = Colors.white,
+      this.icon,
+      this.iconAlignment = Alignment.topRight,
+      this.borderSize = 2,
+      this.unselectedOpacity = 0.5,
+      this.opacityAnimationDuration = 600,
+      this.child});
+}
+
+class _SelectableContainerState extends State<SelectableContainer> {
+  bool _selected = false;
+
+  void assingDefaultValues() {
+    var theme = Theme.of(context);
+
+    this.widget.selectedBackgroundColor =
+        this.widget.selectedBackgroundColor == null
+            ? theme.dialogBackgroundColor
+            : this.widget.selectedBackgroundColor;
+    this.widget.unselectedBackgroundColor =
+        this.widget.unselectedBackgroundColor == null
+            ? theme.dialogBackgroundColor
+            : this.widget.unselectedBackgroundColor;
+    this.widget.selectedBorderColor = this.widget.selectedBorderColor == null
+        ? theme.primaryColor
+        : this.widget.selectedBorderColor;
+    this.widget.unselectedBorderColor =
+        this.widget.unselectedBorderColor == null
+            ? theme.primaryColorDark
+            : this.widget.unselectedBorderColor;
+
+    this.widget.icon =
+        this.widget.icon == null ? Icons.check : this.widget.icon;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    assingDefaultValues();
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _selected = !_selected;
+        });
+        widget.onPressed();
+      },
+      child: AnimatedOpacity(
+        opacity: _selected ? 1.0 : widget.unselectedOpacity,
+        duration: Duration(milliseconds: widget.opacityAnimationDuration),
+        child: Container(
+          child: Column(
+            children: [
+              Stack(
+                alignment: widget.iconAlignment,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(widget.iconSize / 2),
+                    child: widget.child,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                            color: _selected
+                                ? widget.selectedBorderColor
+                                : widget.unselectedBorderColor,
+                            width: widget.borderSize.toDouble()),
+                        borderRadius: BorderRadius.circular(10),
+                        color: _selected
+                            ? widget.selectedBackgroundColor
+                            : widget.unselectedBackgroundColor),
+                  ),
+                  Visibility(
+                    visible: _selected,
+                    child: Container(
+                      padding: EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white),
+                        shape: BoxShape.circle,
+                        color: widget.selectedBorderColor,
+                      ),
+                      child: Icon(
+                        widget.icon,
+                        size: widget.iconSize.toDouble(),
+                        color: widget.iconColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
